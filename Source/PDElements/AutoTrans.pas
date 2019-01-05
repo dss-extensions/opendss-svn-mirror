9283d7f2b34b0d6158204390657c98b953858def
rdugan <rdugan@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-01-05 15:33:00 +0000

    Fix divide-by-zero bug in AutoTrans, V8
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2464 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/PDElements/AutoTrans.pas
+++ Source/PDElements/AutoTrans.pas
@@ -1236,6 +1236,8 @@ begin
                     else
                         kVseries := kVLL - Winding^[2].kVLL;
                     end;
+                    if kVSeries = 0.0 then
+                        kVSeries := kVLL * 0.0001; // In case series has same voltage as common
                     VBase := kVseries * 1000.0;
                 end;
             end;
