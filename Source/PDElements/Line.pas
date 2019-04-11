8c0f4db1a4d99dee55d28b430f419a6b9ec298b9
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-04-11 14:54:34 +0000

    Adding fix for TLine object to correct the way property values are delivered due to the inclusion of 2 new properties for PDElement (Seasons, Ratings)
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@2586 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/PDElements/Line.pas
+++ Source/PDElements/Line.pas
@@ -1462,11 +1462,11 @@ begin
         end;
 
            // Intercept FaultRate, PctPerm, and HourstoRepair
-        30:
-            Result := Format('%-g', [FaultRate]);
-        31:
-            Result := Format('%-g', [PctPerm]);
         32:
+            Result := Format('%-g', [FaultRate]);
+        33:
+            Result := Format('%-g', [PctPerm]);
+        34:
             Result := Format('%-g', [HrsToRepair]);
 
 
