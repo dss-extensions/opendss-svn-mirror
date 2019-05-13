122ef88f1f539cc129b92d4cd325e497b8949b7e
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2019-05-13 20:46:57 +0000

    testing PV daily simulation by FNCS
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@2600 d8739450-1e93-4ef4-a0af-c327d92816ff

--- trunk/Version7/Source/CMD/test/test_fncs.sh
+++ trunk/Version7/Source/CMD/test/test_fncs.sh
@@ -2,5 +2,3 @@
 (exec fncs_player 6h opendss.player &> player.log &)
 (export FNCS_CONFIG_FILE=tracer.yaml && exec fncs_tracer 6h tracer.out &> tracer.log &)
 (export FNCS_CONFIG_FILE=opendss.yaml && exec ./opendsscmd -f 6h &> opendss.log &)
-
-
