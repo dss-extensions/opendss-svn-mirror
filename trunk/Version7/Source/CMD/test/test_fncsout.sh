ab0f945010b661f4fab4329172fc27280b54e9e9
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2020-07-08 06:37:51 +0000

    speeding up the FNCS output
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@2897 d8739450-1e93-4ef4-a0af-c327d92816ff

--- trunk/Version7/Source/CMD/test/test_fncsout.sh
+++ trunk/Version7/Source/CMD/test/test_fncsout.sh
@@ -1,4 +1,4 @@
 (exec fncs_broker 3 &> broker.log &)
 (exec fncs_player 25h opendss.playerout &> player.log &)
-(export FNCS_CONFIG_FILE=tracerout.yaml && exec fncs_tracer 25h tracerout.out &)
-(export FNCS_CONFIG_FILE=opendss.yaml && exec ./opendsscmd -f 25h &> opendss.log &)
+(export FNCS_CONFIG_FILE=tracerout.yaml && exec fncs_tracer 25h tracerout.out &> tracer.log &)
+(export FNCS_CONFIG_FILE=opendss.yaml && exec ./opendsscmd -f 25h &)
