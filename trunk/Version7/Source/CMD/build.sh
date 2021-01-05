39a7f7c94a39d81b23edad61850d59ca97293111
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-01-05 23:33:28 +0000

    updating the Mac OS X build for new cmake files
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@2970 d8739450-1e93-4ef4-a0af-c327d92816ff

--- trunk/Version7/Source/CMD/build.sh
+++ trunk/Version7/Source/CMD/build.sh
@@ -2,5 +2,5 @@ OS="`uname`"
 if [[ "$OS" == "Linux" ]]; then
 	ppcx64 @linuxopts.cfg -B opendsscmd.lpr
 elif [[ "$OS" == "Darwin" ]]; then
-	ppcx64 @fpcopts.cfg -B opendsscmd.lpr
+	ppcx64 @darwinopts.cfg -B opendsscmd.lpr
 fi
