cc159f93c61b41f6fc9e6f45b51c4297ec86384d
celsorocha <celsorocha@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-02-08 22:36:00 +0000

    Fixing typo in LineTypeList
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3024 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/PDElements/Line.pas
+++ Source/PDElements/Line.pas
@@ -197,7 +197,7 @@ begin
     CommandList.Abbrev := true;
 
     LineTypeList := TCommandList.Create(
-        ['OH', 'UG', 'UG_TS', 'UG_CN', 'SWT_LDBRK', 'SWT_FUSE', 'SWT_SECT', 'SWT_REC', 'SWT1_DISC', 'SWT_BRK', 'SWT_ELBOW']);
+        ['OH', 'UG', 'UG_TS', 'UG_CN', 'SWT_LDBRK', 'SWT_FUSE', 'SWT_SECT', 'SWT_REC', 'SWT_DISC', 'SWT_BRK', 'SWT_ELBOW']);
     LineTypeList.Abbrev := true;  // Allow abbreviations for line type code
 
 end;
