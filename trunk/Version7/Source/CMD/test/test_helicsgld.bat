8bd25f440690546c4bfbeb492992f442967c26b0
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-03-12 06:05:17 +0000

    removing debug output for release
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@3102 d8739450-1e93-4ef4-a0af-c327d92816ff

--- trunk/Version7/Source/CMD/test/test_helicsgld.bat
+++ trunk/Version7/Source/CMD/test/test_helicsgld.bat
@@ -1,6 +1,6 @@
-start /b cmd /c helics_broker -f 4 --name=mainbroker --loglevel=7 --debugging ^>helicsbrokergld.log 2^>^&1
-start /b cmd /c helics_player --input=gldopendsshelics.playergld --local --time_units=ns --stop 90000s --loglevel=7 --debugging ^>helicsplayergld.log 2^>^&1
-start /b cmd /c helics_recorder --input=gldrecorderhelics.json --stop 90000s --loglevel=7 --debugging ^>helicsrecordergld.log 2^>^&1
+start /b cmd /c helics_broker -f 4 --name=mainbroker ^>helicsbrokergld.log 2^>^&1
+start /b cmd /c helics_player --input=gldopendsshelics.playergld --local --time_units=ns --stop 90000s ^>helicsplayergld.log 2^>^&1
+start /b cmd /c helics_recorder --input=gldrecorderhelics.json --stop 90000s ^>helicsrecordergld.log 2^>^&1
 set HELICS_CONFIG_FILE=gldopendsshouseshelics.json
 start /b cmd /c opendsscmd -l 25h ^>helicsopendssgld.log 2^>^&1
 set HELICS_CONFIG_FILE=
