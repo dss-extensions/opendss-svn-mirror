843fe64951706a1f87f9fca3895ea20117e3b99a
temcdrm <temcdrm@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-03-12 04:23:54 +0000

    reduce the debug output for release
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@3101 d8739450-1e93-4ef4-a0af-c327d92816ff

--- trunk/Version7/Source/CMD/test/test_helics.sh
+++ trunk/Version7/Source/CMD/test/test_helics.sh
@@ -1,4 +1,4 @@
-(exec helics_broker -f 3 --name=mainbroker --loglevel=7 --debugging &> helicsbroker.log &)
-(exec helics_player --input=opendsshelics.player --local --time_units=ns --stop 21600s --loglevel=7 --debugging &> helicsplayer.log &)
-(exec helics_recorder --input=recorder.json --period 1s --stop 21600s --loglevel=7 --debugging &> helicsrecorder.log &)
+(exec helics_broker -f 3 --name=mainbroker &> helicsbroker.log &)
+(exec helics_player --input=opendsshelics.player --local --time_units=ns --stop 21600s &> helicsplayer.log &)
+(exec helics_recorder --input=recorder.json --period 1s --stop 21600s &> helicsrecorder.log &)
 (export HELICS_CONFIG_FILE=opendss.json && exec ./opendsscmd -l 6h &> helicsopendss.log &)
--- trunk/Version7/Source/CMD/test/test_helicspv.sh
+++ trunk/Version7/Source/CMD/test/test_helicspv.sh
@@ -1,5 +1,5 @@
-(exec helics_broker -f 3 --name=mainbroker --loglevel=7 --debugging &> helicsbrokerpv.log &)
-(exec helics_player --input=opendsshelics.playerpv --local --time_units=ns --stop 90000s --loglevel=7 --debugging &> helicsplayerpv.log &)
-(exec helics_recorder --input=recorderpv.json --period 0.000000001s --stop 90000s --loglevel=7 --debugging&> helicsrecorderpv.log &)
+(exec helics_broker -f 3 --name=mainbroker &> helicsbrokerpv.log &)
+(exec helics_player --input=opendsshelics.playerpv --local --time_units=ns --stop 90000s &> helicsplayerpv.log &)
+(exec helics_recorder --input=recorderpv.json --period 0.000000001s --stop 90000s &> helicsrecorderpv.log &)
 (export HELICS_CONFIG_FILE=opendss.json && exec ./opendsscmd -l 25h &> helicsopendsspv.log &)
 
