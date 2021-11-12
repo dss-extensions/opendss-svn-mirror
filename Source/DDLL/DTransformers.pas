d745a6b699fd74bf561a3a638b15093785f71469
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2021-11-12 14:06:20 +0000

    Updating compilation including core type labeling update
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3265 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/DDLL/DTransformers.pas
+++ Source/DDLL/DTransformers.pas
@@ -167,7 +167,7 @@ begin
                     5:
                         elem.strCoreType := '5-leg';
                     9:
-                        elem.strCoreType := '1-phase-core-form';
+                        elem.strCoreType := 'core-1-phase';
                 else
                     elem.strCoreType := 'shell';
                 end;
