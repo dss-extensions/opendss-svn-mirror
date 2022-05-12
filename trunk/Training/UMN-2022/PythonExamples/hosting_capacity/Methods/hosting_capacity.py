6f2fb3cb69169ca02fe926d4afd0752ba23bf7b0
pauloradatz <pauloradatz@d8739450-1e93-4ef4-a0af-c327d92816ff>
2022-05-12 17:57:06 +0000

    Python examples and ppt
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@3447 d8739450-1e93-4ef4-a0af-c327d92816ff

---
+++ trunk/Training/UMN-2022/PythonExamples/hosting_capacity/Methods/hosting_capacity.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/6/2021 3:00 PM
+# @Author  : Paulo Radatz
+# @Email   : pradatz@epri.com
+# @File    : Methods.py
+# @Software: PyCharm
+
+
+class HostingCapacity:
+
+    def __init__(self, dss, bus, load_mult):
+        self.dss = dss
+        self.bus = bus
+        self.load_mult = load_mult
+
+    def ov_criteria_hc_calc(self, gen_kv, ov_threshold_pu, p_step, max_p=10000):
+        self.__new_gen(gen_kv, p_step)
+
+        i = 0
+        ov_violation = False
+
+        self.dss.text(f"set loadmult={self.load_mult}")
+        self.dss.text("solve")
+        # self.dss.text("Plot profile phases=all")
+        while not ov_violation and i * p_step <= max_p:
+            i += 1
+            self.__increment_generator_size(p_step * i)
+            self.dss.text("solve")
+            # self.dss.text("Plot profile phases=all")
+            max_v = self.__get_max_feeder_voltage()
+
+            if max_v > ov_threshold_pu:
+                ov_violation = True
+
+        if ov_violation:
+            return (i - 1) * p_step
+        else:
+            return max_p
+
+    def __new_gen(self, gen_kv, kw):
+        self.dss.text(f"new generator.gen "
+                      f"phases=3 "
+                      f"kv={gen_kv} "
+                      f"bus1={self.bus} "
+                      f"kw={kw} "
+                      f"kva={kw} "
+                      f"pf=1")
+
+    def __increment_generator_size(self, kw):
+        self.dss.text(f"edit generator.gen "
+                      f"kw={kw} "
+                      f"kva={kw}")
+
+    def __get_max_feeder_voltage(self):
+        voltages = self.dss.circuit_all_bus_vmag_pu()
+        return max(voltages)
+
+
+
---
+++ trunk/Training/UMN-2022/PythonExamples/hosting_capacity/process_hc.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+# @Time    : 8/6/2021 3:41 PM
+# @Author  : Paulo Radatz
+# @Email   : pradatz@epri.com
+# @File    : process_hc.py
+# @Software: PyCharm
+
+import py_dss_interface  # YouTube videos: https://www.youtube.com/playlist?list=PLhdRxvt3nJ8zlzp6b_-7s3_YwwlunTNRC
+import pathlib
+import os
+import math
+import pandas as pd
+from Methods import HostingCapacity
+
+
+def set_baseline():
+    dss.text("Set Maxiterations=100")
+    dss.text("edit Reactor.MDV_SUB_1_HSB x=0.0000001")
+    dss.text("edit Transformer.MDV_SUB_1 %loadloss=0.0000001 xhl=0.00000001")
+    dss.text("Set controlmode=Off")
+
+
+def get_3ph_mv_buses():
+    mv_buses = list()
+    mv_bus_voltage_dict = dict()
+
+    buses = dss.circuit_all_bus_names()
+
+    for bus in buses:
+        dss.circuit_set_active_bus(bus)
+        if bus == "sourcebus":
+            pass
+        elif dss.bus_kv_base() >= 1.0 and len(dss.bus_nodes()) == 3:
+            mv_buses.append(bus)
+            mv_bus_voltage_dict[bus] = dss.bus_kv_base() * math.sqrt(3)
+
+    return mv_buses, mv_bus_voltage_dict
+
+
+script_path = os.path.dirname(os.path.abspath(__file__))
+dss_file = pathlib.Path(script_path).joinpath("Feeders", "ckt5", "Master_ckt5.dss")
+
+dss = py_dss_interface.DSSDLL()
+
+dss.text(f"Compile [{dss_file}]")
+mv_buses, mv_bus_voltage_dict = get_3ph_mv_buses()
+
+bus_name_list = []
+hc_list = []
+for bus in mv_buses:
+    dss.text(f"Compile [{dss_file}]")
+    set_baseline()
+    bus_name_list.append(bus)
+    hc_obj = HostingCapacity(dss, bus, 1)
+    p_max = hc_obj.ov_criteria_hc_calc(mv_bus_voltage_dict[bus], 1.05, 250)
+    hc_list.append(p_max)
+
+# Save results in a csv file
+dict_to_df = dict()
+dict_to_df["Bus_name"] = bus_name_list
+dict_to_df["Hosting_Capacity_kW"] = hc_list
+
+df = pd.DataFrame().from_dict(dict_to_df)
+
+output_file = pathlib.Path(script_path).joinpath("outputs", "results.csv")
+df.to_csv(output_file, index=False)
+
+
