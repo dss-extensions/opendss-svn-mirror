3f0e0cc19fb0e817383829309413d7df4aa559d0
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2024-08-23 22:49:49 +0000

    Added WindGen Model, it works fine in QSTS, still needs to be tested for dynamics.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@3762 d8739450-1e93-4ef4-a0af-c327d92816ff

--- VersionC/PCElements/WindGenVars.h
+++ VersionC/PCElements/WindGenVars.h
@@ -12,7 +12,7 @@ namespace WindGenVars
 
 /*
   ----------------------------------------------------------
-  Copyright (c) 2008-2022, Electric Power Research Institute, Inc.
+  Copyright (c) 2008-2024, Electric Power Research Institute, Inc.
   All rights reserved.
   ----------------------------------------------------------
 
@@ -29,44 +29,58 @@ struct TWindGenVars
 {
       /*Direct-Axis voltage magnitude & angle*/         /*present Shaft Power and relative Speed, rad/sec, difference from Synchronous speed, w0*/
                     /*actual speed = Speed + w0*/      /*Per unit mass constant*/      /*Mass constant actual values (Joule-sec/rad*/     /*Actual and per unit damping factors*/   /*machine Reactances, ohms*/   /*machine Reactances, per unit*/     /*Derivatives of Theta and Speed*/   /*history variables for integration*/  /*Target P and Q for power flow solution, watts, vars*/
-	double Theta;
-	double Pshaft;
-	double Speed;
-	double w0;
-	double Hmass;
-	double Mmass;
-	double D;
-	double Dpu;
-	double kVArating;
-	double kVWindGenBase;
-	double XD;
-	double Xdp;
-	double Xdpp;
-	double puXd;
-	double puXdp;
-	double puXdpp;
-	double dTheta;
-	double dSpeed;
-	double ThetaHistory;
-	double SpeedHistory;
-	double Pnominalperphase;
-	double Qnominalperphase;    /* All Doubles */
+	double	Theta,
+			Pshaft,
+			Speed,
+			w0,
+			Hmass,
+			Mmass,
+			D,
+			Dpu,
+			kVArating,
+			kVWindGenBase,
+			XD,
+			Xdp,
+			Xdpp,
+			puXd,
+			puXdp,
+			puXdpp,
+			dTheta,
+			dSpeed,
+			ThetaHistory,
+			SpeedHistory,
+			Pnominalperphase,
+			Qnominalperphase;    /* All Doubles */
 
         /*32-bit integers*/       /*Number of phases*/   /*Total Number of conductors (wye-connected will have 4)*/
-	int NumPhases;
-	int NumConductors;
-	int Conn;   // 0 = wye; 1 = Delta
+	int		NumPhases,
+			NumConductors,
+			Conn;			// 0 = wye; 1 = Delta
 	
         /* Revisons (additions) to structure ...
           Later additions are appended to end of the structure so that
           previously compiled DLLs do not break
           */
-	double VthevMag;    /*Thevinen equivalent voltage for dynamic model*/
-	double VThevHarm;    /*Thevinen equivalent voltage mag reference for Harmonic model*/
-	double ThetaHarm;    /*Thevinen equivalent voltage angle reference for Harmonic model*/
-	double VTarget;   // Target voltage for WindGen with voltage control
-	Ucomplex::complex Zthev;
-	double XRdp;  // Assumed X/R for Xd'
+	double	VthevMag,		/*Thevinen equivalent voltage for dynamic model*/
+			VThevHarm,		/*Thevinen equivalent voltage mag reference for Harmonic model*/
+			ThetaHarm,		/*Thevinen equivalent voltage angle reference for Harmonic model*/
+			VTarget;		// Target voltage for WindGen with voltage control
+	complex Zthev;
+	double	XRdp;			// Assumed X/R for Xd'
+    String  PLoss;			// Name of the XY curve describing the active power losses for the turbine
+    double  ag,				// Garbox ratio
+            Cp,				// Turbine performance coefficient
+            Lamda,			// Tip speed ratio
+            Poles,			// Number of poles of the induction generator
+            pd,				// Air density
+            Rad,			// Rotor radius
+            VCutin,			// Cut-in speed for the wind generator
+            VCutout,		// Cut-out speed for the wind generator
+            Pm,				// mechanical power (steady-state)
+            Ps,				// Stator active power
+            Pr,				// Rotor active power
+            Pg,				// Total power output
+            s;				// generator pitch
 };
 #pragma pack (pop)
 
