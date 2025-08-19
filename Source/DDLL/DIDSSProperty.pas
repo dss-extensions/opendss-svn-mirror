a52aa0f01bb98192bb7e78591552dae74055c6b3
aovallev <aovallev@d8739450-1e93-4ef4-a0af-c327d92816ff>
2025-08-19 11:44:42 +0000

    Enhancements and bugfixes to Direct DLL DSS Properties interface to mimic COM interface behavior. Includes addition of utility function `TryStrToIntAux` in C++ version as the existing `TryStrToInt` doesn't match Delphi version behavior.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@4068 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/DDLL/DIDSSProperty.pas
+++ Source/DDLL/DIDSSProperty.pas
@@ -1,4 +1,6 @@
 unit DIDSSProperty;
+// DSSProperties Interface
+// This interface implements the DSSproperties (IDSSProperties) interface of OpenDSS by declaring 4 procedures for accessing the different properties included in this interface.
 
 interface
 function DSSProperties(mode:longint; arg:pAnsiChar):pAnsiChar; cdecl;
@@ -11,36 +13,83 @@ var
   FPropIndex   :Integer;
 
 function DSSProperties(mode:longint; arg:pAnsiChar):pAnsiChar; cdecl;
+var TempPropIndex   :Integer;
+    prop_name:String;
 begin
   Result := pAnsiChar(AnsiString('')); // Default return value
-  FPropIndex := StrToInt(string(arg));
   case mode of
     0: begin                                           // DSSProperties.Name
+      if not TryStrToInt(string(arg), TempPropIndex) then
+          TempPropIndex := -1;  // If this is the case, we are not getting an argument or it is not numeric. Leaving this here temporarily for backwards compatibility. This must be decrecated soon (06/17/2025).
+
+      if TempPropIndex > 0 then  // Leaving this here temporarily for backwards compatibility. This must be decrecated soon (06/17/2025).
+         FPropIndex := TempPropIndex + 0;
+
       If (ActiveCircuit[ActiveActor]<> Nil) and (FPropIndex <> 0) {and (FPropClass <> Nil)} Then
         With  ActiveDSSObject[ActiveActor].ParentClass   Do
-        If FPropIndex <= NumProperties Then
+        If (FPropIndex <= NumProperties) and (FPropIndex > 0) Then
           Result := pAnsiChar(AnsiString(PropertyName^[FPropIndex]));
     end;
     1: begin                                           // DSSProperties.Description
+      if not TryStrToInt(string(arg), TempPropIndex) then
+          TempPropIndex := -1;  // If this is the case, we are not getting an argument or it is not numeric. Leaving this here temporarily for backwards compatibility. This must be decrecated soon (06/17/2025).
+
+      if TempPropIndex > 0 then  // Leaving this here temporarily for backwards compatibility. This must be decrecated soon (06/17/2025).
+         FPropIndex := TempPropIndex + 0;
+
       If (ActiveCircuit[ActiveActor]<> Nil) and (FPropIndex <> 0) {and (FPropClass <> Nil)} Then
       With  ActiveDSSObject[ActiveActor].ParentClass Do
-        If FPropIndex <= NumProperties Then
+        If (FPropIndex <= NumProperties) and (FPropIndex > 0) Then
           Result := pAnsiChar(AnsiString(PropertyHelp^[FPropIndex]));
     end;
-    2: begin                                           // DSSProperties.Value - read
-      If (ActiveCircuit[ActiveActor]<> Nil)
+    2: begin                                           // DSSProperties.Val - read
+      if not TryStrToInt(string(arg), TempPropIndex) then
+          TempPropIndex := -1;  // If this is the case, we are not getting an argument or it is not numeric. Leaving this here temporarily for backwards compatibility. This must be decrecated soon (06/17/2025).
+
+      if TempPropIndex > 0 then  // Leaving this here temporarily for backwards compatibility. This must be decrecated soon (06/17/2025).
+         FPropIndex := TempPropIndex + 0;
+
+      If (ActiveCircuit[ActiveActor]<> Nil) and (FPropIndex <> 0)
       THEN  With ActiveDSSObject[ActiveActor] Do
-        If FPropIndex <= ParentClass.NumProperties Then
+        If (FPropIndex <= ParentClass.NumProperties) and (FPropIndex > 0) Then
               Result := pAnsiChar(AnsiString(PropertyValue[ParentClass.PropertyIdxMap^[FPropIndex]]));
     end;
-    3: begin                                           // DSSProperties.Value - Write
-       If (ActiveCircuit[ActiveActor]<> Nil)
+    3: begin                                           // DSSProperties.Val - write
+       If (ActiveCircuit[ActiveActor]<> Nil) and (FPropIndex <> 0)
         THEN  With ActiveDSSObject[ActiveActor] Do
-          If FPropIndex <= ParentClass.NumProperties Then
+          If (FPropIndex <= ParentClass.NumProperties) and (FPropIndex > 0) Then
                 DSSExecutive[ActiveActor].Command := 'Edit ' + ParentClass.Name + '.' + Name + ' ' +
                        ParentClass.PropertyName^[FPropIndex] + '=' +
                        string(arg);
                 Result:=pAnsiChar(AnsiString(''));
+    end;
+    4: begin                                           // DSSProperties.ActiveProperty - read
+       Result:=pAnsiChar(AnsiString(IntToStr(FPropIndex)));
+    end;
+    5: begin                                           // DSSProperties.ActiveProperty - write
+
+       if not TryStrToInt(string(arg), TempPropIndex) then
+           TempPropIndex := -1;
+
+       FPropIndex := 0;
+       If (ActiveCircuit[ActiveActor]<> Nil)
+        THEN  With ActiveDSSObject[ActiveActor] Do
+         If TempPropIndex > -1 then
+           begin
+             If (TempPropIndex <= ParentClass.NumProperties ) and (TempPropIndex > 0) Then
+                FPropIndex := TempPropIndex + 0;
+           end
+         else begin
+            TempPropIndex := 0;
+            prop_name := string(arg);
+             With ParentClass Do
+             For TempPropIndex := 1 to ParentClass.NumProperties Do Begin
+                 If CompareText(prop_name, PropertyName^[TempPropIndex]) = 0 Then Begin
+                     FPropIndex := TempPropIndex + 0;
+                     Break;
+                 End;
+             End;
+         end;
     end
     else
       Result:=pAnsiChar(ansistring(''));
