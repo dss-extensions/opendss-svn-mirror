b7078bd42834451f22f545aa0f55b56604d871d5
celsorocha <celsorocha@d8739450-1e93-4ef4-a0af-c327d92816ff>
2025-09-25 13:37:58 +0000

    Adjusting modification from revision 4057 to force a new power flow only in the first iteration of the control loop. Condition added to accommodate edge cases in which peakshave band is lower or close to the controlled storage idling losses. At those cases, storage controller logic would get stuck, reaching maximum number of iterations.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@4078 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Source/Controls/StorageController.pas
+++ Source/Controls/StorageController.pas
@@ -1847,7 +1847,8 @@ begin
                     if not (FleetState = STORE_DISCHARGING) then
                     begin
                         SetFleetToDischarge;
-                        StorekWChanged := true;  // if not already discharging, force new power flow.
+                        if ActiveCircuit[ActorID].Solution.ControlIteration = 1 then
+                            StorekWChanged := true; // if not already discharging, force new power flow.
                     end;
                     if ShowEventLog then
                         AppendToEventLog('StorageController.' + Self.Name, Format('Attempting to dispatch %-.6g kW with %-.6g kWh remaining and %-.6g kWh reserve.', [kWNeeded, RemainingkWh, ReservekWh]), ActorID);
@@ -2119,7 +2120,8 @@ begin
                     if not (FleetState = STORE_CHARGING) then
                     begin
                         SetFleetToCharge;
-                        StorekWChanged := true;  // if not already charging, force new power flow.
+                        if ActiveCircuit[ActorID].Solution.ControlIteration = 1 then
+                            StorekWChanged := true; // if not already charging, force new power flow.
                     end;
   //                       If ShowEventLog Then  AppendToEventLog('StorageController.' + Self.Name, Format('Attempting to charge %-.6g kW with %-.6g kWh remaining and %-.6g rating.', [kWNeeded, (TotalRatingkWh-ActualkWh), TotalRatingkWh]), ActorID);
                     if ShowEventLog then
