0e183f417eee714e5e44a8bd8922697f4dc91b21
aovallev <aovallev@d8739450-1e93-4ef4-a0af-c327d92816ff>
2026-01-27 17:04:37 +0000

    Minor fix on "units" property description. Clean up on C++ side.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code/trunk@4129 d8739450-1e93-4ef4-a0af-c327d92816ff

--- Version8/Source/PDElements/Line.pas
+++ Version8/Source/PDElements/Line.pas
@@ -294,9 +294,9 @@ Begin
      PropertyHelp^[19] := 'Geometry code for LineGeometry Object. Supercedes any previous definition of line impedance. ' +
                          'Line constants are computed for each frequency change or rho change. CAUTION: may alter number of phases. '+
                          'You cannot subsequently change the number of phases unless you change how the line impedance is defined.';
-     PropertyHelp^[20] := 'Length Units = {none | mi|kft|km|m|Ft|in|cm } Default is None - assumes length units match impedance units. '
-						'This property is reset to its default value when impedances are specified or edited. Make sure to specify ' + 
-						'desired value after specifying or editing any impedance property.';
+     PropertyHelp^[20] := 'Length Units = {none | mi|kft|km|m|Ft|in|cm } Default is None - assumes length units match impedance units. ' +
+                          'This property is reset to its default value when impedances are specified or edited. Make sure to specify ' +
+                          'desired value after specifying or editing any impedance property.';
      PropertyHelp^[21] := 'Reference to a LineSpacing for use in a line constants calculation.' + CRLF +
                           'Must be used in conjunction with the Wires property.' + CRLF +
                           'Specify this before the wires property.';
