850dd8b953372d4920037608a5b81728983d7340
davismont <davismont@d8739450-1e93-4ef4-a0af-c327d92816ff>
2026-07-20 21:31:58 +0000

    Adding voltage ride-through for IBR including Storage. Documentation updated and operational states refined.
    git-svn-id: https://svn.code.sf.net/p/electricdss/code@4167 d8739450-1e93-4ef4-a0af-c327d92816ff

--- trunk/Version8/Source/Controls/InvControl.pas
+++ trunk/Version8/Source/Controls/InvControl.pas
@@ -430,11 +430,10 @@ const
     WATTVAR   = 5;
     AVR       = 6;
     GFM       = 7;
-    VRIDE     = 8;
 
     // Modes in string type
-    myCtrlModes   : array [0..7] of string =
-    ('voltvar', 'voltwatt', 'dynamicreaccurr', 'wattpf', 'wattvar', 'avr', 'gfm', 'vride');
+    myCtrlModes   : array [0..6] of string =
+    ('voltvar', 'voltwatt', 'dynamicreaccurr', 'wattpf', 'wattvar', 'avr', 'gfm');
 
     myDERTypes    : array [0..1] of string =
     ('PVSystem', 'Storage');
@@ -519,7 +518,7 @@ procedure TInvControl.DefineProperties;
                       'No capability of hierarchical control between two controls for a single element is implemented at this time.';
 
     PropertyHelp^[2] := 'Smart inverter function in which the InvControl will control the PC elements specified in DERList, according to the options below:' +CRLF+CRLF+
-                      'Must be one of: {VOLTVAR* | VOLTWATT | DYNAMICREACCURR | WATTPF | WATTVAR | GFM | VRIDE} ' +CRLF+
+                      'Must be one of: {VOLTVAR* | VOLTWATT | DYNAMICREACCURR | WATTPF | WATTVAR | GFM} ' +CRLF+
                       'if the user desires to use modes simultaneously, then set the CombiMode property. Setting the Mode to any valid value disables combination mode.'+
 
                        CRLF+CRLF+'In volt-var mode (Default). This mode attempts to CONTROL the vars, according to one or two volt-var curves, depending on the monitored voltages, present active power output, and the capabilities of the PVSystem/Storage. ' +
@@ -527,8 +526,7 @@ procedure TInvControl.DefineProperties;
                        CRLF+CRLF+'In dynamic reactive current mode. This mode attempts to increasingly counter deviations by CONTROLLING vars, depending on the monitored voltages, present active power output, and the capabilities of the of the PVSystem/Storage.'+
                        CRLF+CRLF+'In watt-pf mode. This mode attempts to CONTROL the vars, according to a watt-pf curve, depending on the present active power output, and the capabilities of the PVSystem/Storage. '+
                        CRLF+CRLF+'In watt-var mode. This mode attempts to CONTROL the vars, according to a watt-var curve, depending on the present active power output, and the capabilities of the PVSystem/Storage. ' +
-                       CRLF+CRLF+'In GFM mode this control will trigger the GFM control routine for the DERs within the DERList. The GFM actiosn will only take place if the pointed DERs are in GFM mode. The controller parameters are locally setup at the DER. ' +
-                       CRLF+CRLF+'VRIDE mode provides support for GFL IBRs to comply with the Voltage ride-through and trip requirements for certified Inverter abnormal operating  Performance-Category III (IEEE 1547) . ';
+                       CRLF+CRLF+'In GFM mode this control will trigger the GFM control routine for the DERs within the DERList. The GFM actiosn will only take place if the pointed DERs are in GFM mode. The controller parameters are locally setup at the DER. ';
 
     PropertyHelp^[3] := 'Combination of smart inverter functions in which the InvControl will control the PC elements in DERList, according to the options below: '+CRLF+CRLF+
                       'Must be a combination of the following: {VV_VW | VV_DRC}. Default is to not set this property, in which case the single control mode in Mode is active.  ' +
@@ -746,7 +744,6 @@ function TInvControl.Edit(ActorID : Integer):Integer;
     StrTemp,
     ParamName,
     Param             : String;
-    arr_temp          : pDoubleArray;
 
     NodeBuffer        : Array[1..10] of Integer;
 
@@ -2086,12 +2083,6 @@ begin
             End;
           End;
         End;
-      end
-
-//---------------------------------------------------------------------------------------------------------------------------------------//
-//    Grid forming inverter
-      else if(ControlMode = VRIDE) then
-      begin
 
       end;
 
